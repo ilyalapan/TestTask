d02b4c8548e566c52807397b0a4f3943990e2cf7
Ilya Lapan <lapan.ilya@gmail.com>
2016-12-14 09:08:14 +0300

    Initial Commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by Ilya Lapan on 14/12/2016.
+//  Copyright © 2016 ilyalapan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

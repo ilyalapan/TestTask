980348ae3807531d590eb66edea47a37dc3d66ba
Ilya Lapan <lapan.ilya@gmail.com>
2016-12-14 10:15:17 +0300

    Initial commit

---
+++ TestTask/Product.swift
@@ -0,0 +1,9 @@
+//
+//  Product.swift
+//  TestTask
+//
+//  Created by Ilya Lapan on 14/12/2016.
+//  Copyright © 2016 ilyalapan. All rights reserved.
+//
+
+import Foundation
---
+++ TestTask/ProductListManager.swift
@@ -0,0 +1,9 @@
+//
+//  ProductListManager.swift
+//  TestTask
+//
+//  Created by Ilya Lapan on 14/12/2016.
+//  Copyright © 2016 ilyalapan. All rights reserved.
+//
+
+import Foundation
